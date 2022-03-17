55d995a80232972d991d595d8c5ffe973ba8848a
yuzheng14 <422450117@qq.com>
2022-03-17 20:30:36 +0800

    栈的定义

---
+++ chapter3-stack-queue-array/sqstack.h
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+#if !defined SQSTACK_H
+#define SQSTACK_H
+
+#if !defined ELEMTYPE
+#define ELEMTYPE
+typedef int ElemType;
+#endif
+
+#if defined MaxSize
+#undef MaxSize
+#endif
+
+#define MaxSize 10
+
+#if !defined SQSTACK
+#define SQSTACK
+typedef struct{
+    ElemType data[MaxSize];
+    int top;
+} SqStack;
+#endif
+
+#endif
