524f1a5fe91da9f8ea25308754b9d9ff7b9b28c3
yuzheng14 <422450117@qq.com>
2022-03-17 21:40:46 +0800

    test

--- chapter3-stack-queue-array/listack.c
+++ chapter3-stack-queue-array/listack.c
@@ -43,3 +43,27 @@ bool GetTop(LiStack S, ElemType *e)
         *e = S->next->data;
     return true;
 }
+
+int main (void)
+{
+    LiStack S;
+    InitStack(&S);
+    if(StackEmpty(S))
+        puts("当前栈为空");
+    else 
+        puts("当前栈不为空");
+    Push(&S,1);
+    Push(&S,2);
+    Push(&S,3);
+    Push(&S,4);
+    Push(&S,5);
+    Push(&S,6);
+    int e;
+    while(!StackEmpty(S))
+    {
+        Pop(&S,&e);
+        printf("%-3d",e);
+    }
+    putchar('\n');
+    return 0;
+}
