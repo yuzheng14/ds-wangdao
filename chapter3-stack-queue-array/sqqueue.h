d71f117df7e3644c0f9398219dcd763c847fb445
yuzheng14 <422450117@qq.com>
2022-03-18 19:37:54 +0800

    顺序队列的定义

---
+++ chapter3-stack-queue-array/sqqueue.h
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+#if !defined SQ_QUEUE_H
+#define SQ_QUEUE_H
+
+#if !defined ELEMTYPE
+#define ELEMTYPE
+typedef int ElemType;
+#endif
+
+#ifdef MaxSize
+#undef MaxSize
+#endif
+#define MaxSize 10
+
+#if !defined SQ_QUEUE
+#define SQ_QUEUE
+typedef struct 
+{
+    ElemType data[MaxSize];
+    int font,rear;
+} SqQueue;
+#endif
+
+#endif
