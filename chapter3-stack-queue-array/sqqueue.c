cb4a389a4a8bc809dcbd9e7aa03a71300f7958b6
yuzheng14 <422450117@qq.com>
2022-03-18 21:51:54 +0800

    测试线性队列

--- chapter3-stack-queue-array/sqqueue.c
+++ chapter3-stack-queue-array/sqqueue.c
@@ -38,3 +38,27 @@ bool GetHead(SqQueue Q, ElemType *e)
         *e = Q.data[Q.rear];
     return true;
 }
+
+int main(void)
+{
+    SqQueue Q;
+    InitQueue(&Q);
+    EnQueue(&Q, 1);
+    EnQueue(&Q, 2);
+    EnQueue(&Q, 3);
+    EnQueue(&Q, 4);
+    EnQueue(&Q, 5);
+    EnQueue(&Q, 6);
+    EnQueue(&Q, 7);
+    EnQueue(&Q, 8);
+    EnQueue(&Q, 9);
+    EnQueue(&Q, 10);
+    int e;
+    while (!QueueEmpty(Q))
+    {
+        DeQueue(&Q, &e);
+        printf("%-3d", e);
+    }
+    putchar('\n');
+    return 0;
+}
