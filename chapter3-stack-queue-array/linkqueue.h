bdc13d979b0aba431a10e10fa820a4b58bbe17f2
yuzheng14 <422450117@qq.com>
2022-03-18 20:48:31 +0800

    链式队列的定义

---
+++ chapter3-stack-queue-array/linkqueue.h
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+#if !defined LINK_QUEUE_H
+#define LINK_QUEUE_H
+
+#if !defined ELEMTYPE
+#define ELEMTYPE
+typedef int ElemType;
+#endif
+
+#if !defined LINK_NODE
+#define LINK_NODE
+typedef struct LinkNode
+{
+    ElemType data;
+    struct LinkNode *next;
+} LinkNode;
+#endif
+
+#if !defined LINK_QUEUE
+#define LINK_QUEUE
+typedef struct
+{
+    LinkNode *front,*rear;
+} LinkQueue;
+#endif
+
+#endif
