a4779867b0c16f4e4c1ec9e856b1539ac65fdb0a
yuzheng14 <422450117@qq.com>
2022-03-23 19:32:33 +0800

    树的链式存储定义

---
+++ chapter5-tree/bitree.h
@@ -0,0 +1,17 @@
+#if !defined BITREE_H
+#define BITREE_H
+
+#if defined ELEMTYPE
+#undef ELEMTYPE
+#endif
+
+#define ELEMTYPE
+typedef int ElemType;
+
+typedef struct BiTNode
+{
+    ElemType data;
+    struct BiNode *lchild, *rchild;
+} BiTNode, *BiTree;
+
+#endif
