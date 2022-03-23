872f7fb240b133c1d8c082703ed6fd8c2e6f43bf
yuzheng14 <422450117@qq.com>
2022-03-23 19:22:30 +0800

    树的顺序存储定义

---
+++ chapter5-tree/treenode.h
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+#if !defined TREE_NODE_H
+#define TREE_NODE_H
+
+#if defined ELEMTYPE
+#undef ELEMTYPE
+#endif
+
+#if !defined MAXSIZE
+#define MAXSIZE 100
+#endif
+
+#define ELEMTYPE
+typedef int ElemType;
+
+
+typedef struct TreeNode{
+    ElemType value;
+    bool isEmpty;
+} TreeNode, Tree[MAXSIZE];
+
+#endif
