0e65fd80cf93079f6c95c85e3763298a94bee000
yuzheng14 <422450117@qq.com>
2022-03-29 20:04:28 +0800

    中序线索化

---
+++ chapter5-tree/threadtree.c
@@ -0,0 +1,64 @@
+#include "threadtree.h"
+
+void InOrder(ThreadTree T, void (*visit)(ThreadNode *))
+{
+    if (T)
+    {
+        InOrder(T->lchild, visit);
+        visit(T);
+        InOrder(T->rchild, visit);
+    }
+}
+
+bool CreateThreadTree(ThreadTree *T)
+{
+    ElemType ch;
+    if (scanf("%c", &ch) != EOF)
+    {
+        if (ch == '#')
+            (*T) = NULL;
+        else
+        {
+            (*T) = (ThreadNode *)malloc(sizeof(ThreadNode));
+            if (!(*T))
+                return false;
+            (*T)->data = ch;
+            (*T)->ltag = (*T)->rtag = Link;
+            return CreateThreadTree(&(*T)->lchild) && CreateThreadTree(&(*T)->rchild);
+        }
+    }
+    return true;
+}
+
+ThreadNode *pre;
+
+void thread(ThreadNode *current)
+{
+    if (!current->lchild)
+    {
+        current->lchild = pre;
+        current->ltag = Thread;
+    }
+    if (pre && !pre->rchild)
+    {
+        pre->rchild = current;
+        pre->rtag = Thread;
+    }
+    pre = current;
+}
+
+void InThread(ThreadTree T)
+{
+    InOrder(T, thread);
+}
+
+void CreateInThread(ThreadTree *T)
+{
+    if (*T)
+    {
+        pre = NULL;
+        InThread(*T);
+        if (!pre->rchild)
+            pre->rtag = Thread;
+    }
+}
--- chapter5-tree/threadtree.h
+++ chapter5-tree/threadtree.h
@@ -1,4 +1,6 @@
 #include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #if !defined THREAD_TREE_H
 #define THREAD_TREE_H
@@ -15,7 +17,12 @@ typedef struct ThreadNode
 {
     ElemType data;
     struct ThreadNode *lchild, *rchild;
-    bool ltag, rag;
+    bool ltag, rtag;
 } ThreadNode, *ThreadTree;
+
+void InOrder(ThreadTree T, void (*visit)(ThreadNode *));
+bool CreateThreadTree(ThreadTree *T);
+// void InThread(ThreadTree T);
+void CreateInThread(ThreadTree *T);
 
 #endif
