7477761caaca9bdbd1001f95a94cf8df14df097e
yuzheng14 <422450117@qq.com>
2022-03-31 19:18:29 +0800

    修改遍历的逻辑

--- chapter5-tree/tree.c
+++ chapter5-tree/tree.c
@@ -9,13 +9,9 @@ void PreOrder(CSTree T, void (*visit)(CSNode *))
     if (T)
     {
         visit(T);
-        if (T->firstchild)
-        {
-            CSNode *current = T->firstchild;
+        CSNode *current;
+        for (current = T->firstchild; current; current = current->nextsibling)
             PreOrder(current, visit);
-            for (current = current->nextsibling; current; current = current->nextsibling)
-                PreOrder(current, visit);
-        }
     }
 }
 // 树的后序遍历和二叉树的中序遍历一样
@@ -23,28 +19,24 @@ void PostOrder(CSTree T, void (*visit)(CSNode *))
 {
     if (T)
     {
-        if (T->firstchild)
-        {
-            CSNode *current = T->firstchild;
+        CSNode *current;
+        for (current = T->firstchild; current; current = current->nextsibling)
             PostOrder(current, visit);
-            for (current = current->nextsibling; current; current = current->nextsibling)
-                PostOrder(current, visit);
-        }
         visit(T);
     }
 }
 
-void LevelOrder(CSTree T,void (*visit)(CSNode *))
+void LevelOrder(CSTree T, void (*visit)(CSNode *))
 {
     LinkQueue Q;
     InitQueue(&Q);
-    EnQueue(&Q,T);
+    EnQueue(&Q, T);
     CSNode *current;
-    while(!QueueEmpty(Q))
+    while (!QueueEmpty(Q))
     {
-        DeQueue(&Q,&current);
+        DeQueue(&Q, &current);
         visit(current);
-        for(current=current->firstchild;current;current=current->nextsibling)
-            EnQueue(&Q,current);
+        for (current = current->firstchild; current; current = current->nextsibling)
+            EnQueue(&Q, current);
     }
 }
