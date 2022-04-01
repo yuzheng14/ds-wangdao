ae60cd2a44fe3a50780f6731c7a029ccb794ddb8
yuzheng14 <422450117@qq.com>
2022-04-01 21:22:04 +0800

    略微修改定义

--- chapter6-graph/matrixgraph.h
+++ chapter6-graph/matrixgraph.h
@@ -1,7 +1,12 @@
+#include <limits.h>
 #if !defined MATRIX_GRAPH_H
 #define MATRIX_GRAPH_H
 
 #define MaxVertexNum 100
+#define INF INT_MAX
+
+typedef char VertexType;
+typedef int EdgeType;
 
 typedef struct
 {
