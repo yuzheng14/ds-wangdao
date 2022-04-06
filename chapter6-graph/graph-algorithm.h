09a834fdf4f0ac8095ef5186d34c92e685fc7219
yuzheng14 <422450117@qq.com>
2022-04-06 19:55:26 +0800

    图算法头文件

---
+++ chapter6-graph/graph-algorithm.h
@@ -0,0 +1,16 @@
+#include <stdbool.h>
+#include "../chapter3-stack-queue-array/linkqueue.h"
+#if !defined GRAPH_ALGORITHM_H
+#define GRAPH_ALGORITHM_H
+
+#if !defined ADJLIST_GRAPH_H || !defined MATRIX_GRAPH_H
+#include "matrixgraph.h"
+#endif
+
+bool visited[MaxVertexNum];
+LinkQueue Q;
+
+void BFSTraverse(Graph G, void (*visit)(int));
+void BFS(Graph G, int v, void (*visit)(int));
+
+#endif
