d31319357e78776146e802fa6a396ce9c609b424
yuzheng14 <422450117@qq.com>
2022-05-09 19:59:20 +0800

    test

--- test.c
+++ test.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void)
-{
-    int i;
-    for (i = 1; i < 1; i++)
-        printf("%d\n", i);
+int main(void) {
+    printf("%d\n", 1/2);
     return 0;
 }
